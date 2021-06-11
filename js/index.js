a4cb57c8650ceb61063306243cf40ff19f757e29
tufronicolas <ntufro@udesa.edu.ar>
2021-06-11 11:40:11 -0300

    canciones.index

--- js/index.js
+++ js/index.js
@@ -1,34 +1,39 @@
-window.addEventListener('load', function() {
-    
-    let url = 'https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart'
-    
+window.addEventListener('load', function () {
 
-    fetch( url )
-        .then( function(response){
+    let url = 'https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart'
+
+
+    fetch(url)
+        .then(function (response) {
             return response.json();
         })
-        .then( function(data){
+        .then(function (data) {
             //Aca muestro código
             console.log(data);
-            let arrayInfo = data.data
-            let lista = document.querySelector('.lista');
-            let contenidoLista =''; //poner el contenido a mostrar dentro de la lista.
-    
-            for(let i=0; i<arrayInfo.length; i++){
-                contenidoLista += `<li> 
-                                        <a href="detalle.html?id=${arrayInfo[i].id}">
-                                         ${arrayInfo[i].title}
-                                        </a>
-                                    </li>`
+            let lista = document.querySelector('.main-canciones');
+            let contenidoLista = ''; //poner el contenido a mostrar dentro de la lista.
+            for (let i = 0; i < 5; i++) {
+                let imagenCanciones = data.tracks.data[i].album.cover_big;
+                let tituloCanciones = data.tracks.data[i].title;
+                let nombreArtista = data.tracks.data[i].artist.name
+                contenidoLista += `<a href="./detail-track.html">
+                                        <article class="canciones">
+                                            <div class="img-container">
+                                                <img class="index-img" src="${imagenCanciones}" alt="">
+                                            </div>
+                                            <h5 class="titulo-elementos">${tituloCanciones}</h5>
+                                            <p class="texto-elementos">${nombreArtista}</p>
+                                        </article>
+                                    </a>`
             }
-            
+
             lista.innerHTML += contenidoLista
-    
-    
+
+
         })
-        .catch( function(error){
+        .catch(function (error) {
             console.log(error);
         })
 
-    
-}) 
+
+})
