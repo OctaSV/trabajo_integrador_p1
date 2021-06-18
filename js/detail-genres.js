7cb04f60f48725b376cd9cc3598d2254e57ae60c
AraujoJulian <araujoj@udesa.edu.ar>
2021-06-18 11:56:07 -0300

    JS genero y detalle de genero terminadas, cambios css y html

--- js/detail-genres.js
+++ js/detail-genres.js
@@ -1,3 +1,34 @@
-window.addEventListener('load', function() {
-    
-}) 
+window.addEventListener('load', function () {
+
+    let queryString = location.search
+    let queryStringToObject = new URLSearchParams(queryString);
+    let id = queryStringToObject.get('id');
+
+    let url = `https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre/${id}/artists`
+
+    fetch(url)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data.data);
+
+            let lista = document.querySelector('.detailGenres');
+            let contenidoLista = '';
+
+            for (let i = 0; i < data.data.length; i++) {
+                let nombreArtista = data.data[i].name;
+                let imagenArtista = data.data[i].picture_big;
+
+                contenidoLista += `<li><a href="./detail-artist.html"> ${nombreArtista} </a> </li><img src="${imagenArtista}" alt=""></img>`
+
+            }
+            lista.innerHTML += contenidoLista
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+})
+
+
+
--- js/genres.js
+++ js/genres.js
@@ -1,3 +1,65 @@
 window.addEventListener('load', function() {
     
-}) 
+    let url = 'https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre'
+
+    fetch(url)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+
+        let lista = document.querySelector('.genres-1');
+        let contenidoLista = ''; 
+
+        for ( let i=0 ; i<7 ; i++){
+        let nombreGenero = data.data[i].name ; 
+        let id = data.data[i].id
+
+        contenidoLista += `<li><a href="./detail-genres.html?id=${id}"> ${nombreGenero} </a></li>`
+
+        }
+        lista.innerHTML += contenidoLista
+
+        let lista2 = document.querySelector('.genres-2')
+        let contenidoLista2 = '';
+
+        for ( let i=7 ; i<14 ; i++){
+        let nombreGenero = data.data[i].name ;        
+        let id = data.data[i].id
+
+        contenidoLista2 += `<li><a href="./detail-genres.html?id=${id}"> ${nombreGenero} </a></li>`
+    
+        }
+        lista2.innerHTML += contenidoLista2
+        
+        let lista3 = document.querySelector('.genres-3')
+        let contenidoLista3 = '';
+
+        for ( let i=14 ; i<21 ; i++){
+        let nombreGenero = data.data[i].name ; 
+        let id = data.data[i].id       
+        
+        contenidoLista3 += `<li><a href="./detail-genres.html?id=${id}"> ${nombreGenero} </a></li>`
+    
+        }
+        lista3.innerHTML += contenidoLista3
+        
+        let lista4 = document.querySelector('.genres-4')
+        let contenidoLista4 = '';
+
+        for ( let i=21 ; i<28 ; i++){
+        let nombreGenero = data.data[i].name ; 
+        let id = data.data[i].id       
+        
+        contenidoLista4 += `<li><a href="./detail-genres.html?id=${id}"> ${nombreGenero} </a></li>`
+    
+        }
+        lista4.innerHTML += contenidoLista4                
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
+
+
+})
